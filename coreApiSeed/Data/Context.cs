436f9e67888b3007904f6e6cfd5fda0a28f34b92
Vinícius Margotti <viniciusmargotti@hotmail.com>
2021-03-10 21:53:37 -0300

    Initial commit

---
+++ coreApiSeed/Data/Context.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace coreApiSeed.Data
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options): base(options)
+        {
+        }
+
+        public DbSet<Models.Cliente> Cliente { get; set; }
+    }
+}
---
+++ coreApiSeed/Models/Cliente.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace coreApiSeed.Models
+{
+    public class Cliente
+    {
+         [Key]
+        public int ClienteId { get; set; }
+
+        [MaxLength( 50, ErrorMessage ="o nome deve conter no máximo 50 caracteres")]
+        public string nome { get; set; }
+
+        [MaxLength(50, ErrorMessage = "o email deve conter no máximo 100 caracteres")]
+        public string email { get; set; }
+
+        public bool ativo { get; set; }
+    }
+}
